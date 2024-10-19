a4495befa035289beba4bcd9a7b6905bca9dacab
nanig <nanigopaldey00@gmail.com>
2024-10-19 23:57:19 +0530

    add api clint var to val

--- app/src/main/java/com/example/madico/api/api.kt
+++ app/src/main/java/com/example/madico/api/api.kt
@@ -5,6 +5,6 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object api {
-    var clintt = OkHttpClient.Builder().build()
+    val clintt = OkHttpClient.Builder().build()
     var instance = Retrofit.Builder().client(clintt).baseUrl(apibuilder.baseurl).addConverterFactory(GsonConverterFactory.create()).build().create(apibuilder::class.java)
 }
